8b4e75b7e3830bcd8f42e66e662ae2f1889772da
RanaZoro <asamad69000@gmail.com>
2024-09-01 18:38:40 +0500

    Update

--- ranacpp4dsa.cpp
+++ ranacpp4dsa.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+using namespace std;
+
 int main() {
 	int n1;
 	double n2 = 7.9;
@@ -8,6 +10,11 @@ int main() {
 	std::cin >> n1;
 	float n5 = (n2 * n3) / n1;
 	std::cout << "n5 is = " << n5 << std::endl;
+	double n6,n7;
+	std::cout << "Enter Two Doubles = ";
+	std::cin >> n6 >> n7;
+	std::cout << "n6 is = " << n6 << endl << "n7 is = " << n7;
+	
 
 	return 0;
 }
