83edfa84690bdc85da66556e1d88dc8450f0d781
RanaZoro <asamad69000@gmail.com>
2024-09-04 15:59:56 +0500

    Update

--- ranacpp4dsa.cpp
+++ ranacpp4dsa.cpp
@@ -17,9 +17,15 @@ int main() {
 	std::cin >> n6 >> n7;
 	std::cout << "n6 is = " << n6 << endl << "n7 is = " << n7;
 	std::cout << endl << (n6 + n7);
-	cout << n6 % n7;
-	int n9 = n6 += n7;
-	
+//  double%double = error
+// expresion must have integral or unscoped enum type occurs when i apply mod to doubles or floats 
+	int n9 = n6, n10 = n7; //type conversion
+	std::cout << n9 % n10;
+	n6 += n7;
+	float n11 = 9.8 , n12 = 3.14;
+	int n13 = (int)n11; //better method for t.c
+	int n14 = (int)n12;
+	cout << n13 << n14;
 
 	return 0;
 }
